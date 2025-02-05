33111b730e805b0f303fc7e26143bbf1aeaf3c47
Octavio Martinez <omartinez1618@gmail.com>
2025-02-05 16:55:17 -0400

    feat: Mejora del menú y reubicación de pruebas
    Se mejoró la función 'render' del menú para retornar el índice de la opción seleccionada, además de su valor y texto.  Se reubicaron las pruebas del menú a la carpeta 'src/scripts/'.  Se añadieron opciones de configuración a los elementos del menú en el archivo de prueba.
    
    [by Kommiter]

--- src/scripts/test.ts
+++ src/scripts/test.ts
@@ -1,6 +1,6 @@
 import { exit } from "process";
-import { readTerminal, writeTerminal } from "./terminal";
-import createMenu from "./menu";
+import { readTerminal, writeTerminal } from "../terminal";
+import createMenu from "../menu";
 
 const menu = createMenu({
     colorTitle: 'white',
@@ -10,12 +10,12 @@ const menu = createMenu({
     colorOptionHover: 'black',
 })
 
-const [option, value] = await menu
+const [option, , value] = await menu
     .head("[green]Este es el encabezado del menu en verde[/green]")
-    .input(" 1 - Saludo", true)
+    .input(" 1 - Saludo", { clickable:true, defaultSelection:true })
     .item(" 2 - Potencia.")
-    .bool(" 3 - Toggle Test", ["[green]Si[/green]", "[red]No[/red]"])
-    .select(" 4 - Prueba select", ["Opcion 1", "Opcion 2", "Opcion 3", "Opcion 4", "Opcion 5"])
+    .bool(" 3 - Toggle Test", { clickable:true, value:["[green]Si[/green]", "[red]No[/red]"] })
+    .select(" 4 - Prueba select", { clickable:true, value:["Opcion 1", "Opcion 2", "Opcion 3", "Opcion 4", "Opcion 5"]})
     .number(" 5 - Ingresa tu edad", {
         value:5,
         step: 0.5,
@@ -25,18 +25,21 @@ const [option, value] = await menu
     .item(" 5 - Salir.")
     .render()
 
-if( option===1) {
+if( option===0) {
     console.log(`Hola mundo!${value ? ' en especial a '+value  : ''}`)
 }
-else if( option===2) {
+else if( option===1) {
     const base = parseInt(await readTerminal("Ingrese la [red]base[/red]: ") as string)
     const exponente = parseInt(await readTerminal("Ingrese el [green]exponente[/green]: ") as string)
     await writeTerminal(`El resultado de [red]${base}[/red]^[green]${exponente}[/green] es: ${Math.pow(base, exponente)}`)
 }
-else if( option===3) {
+else if( option===2) {
     await writeTerminal(`Bool! ${value}`)
 }
-else if( option===4) {
-    console.log("Adios!")
+else if( option===3) {
+    console.log("Opcion seleccionada: ", value.slice(1)[value[0]] )
+}
+else{
+    console.log('Bye')
 }
 exit(0)
---
+++ src/scripts/vite.ts
@@ -0,0 +1,35 @@
+import { exit } from "process";
+import createMenu from "../menu";
+
+const menu = createMenu({
+    colorTitle: 'white',
+    bgColorOption: '',
+    colorOption: 'white',
+    bgColorOptionHover: 'bgCyanBright',
+    colorOptionHover: 'black',
+})
+
+const templates = [
+    "[yellow]Vanilla[/yellow]",
+    "[green]Vue[/green]",
+    "[blue]React[/blue]",
+    "[magentaBright]Preat[/magentaBright]",
+    "[red]Lit[/red]",
+    "[red]Svelte[/red]",
+    "[blue]Solid[/blue]",
+    "[blue]Qwik[/blue]",
+    "[red]Angular[/red]",
+    "Others",
+]
+
+const [option, value] = await menu
+    .head("[bgBlue][white]Vite Project Launcher[/white][/bgBlue]\n")
+    .input(" Project name", { defaultSelection:true })
+    .bool(" Typescript:", { value:["[green]Si[/green]", "[red]No[/red]"]})
+    .bool(" Init Git:  ", { value:["[green]Si[/green]", "[red]No[/red]"]})
+    .select(" Select a framework:", { value:templates, clickable:false, compat:true })
+    .item(" [blue]Ok[/blue]")
+    .item(" [red]Cancel[/red]")
+    .render()
+
+exit(0)
